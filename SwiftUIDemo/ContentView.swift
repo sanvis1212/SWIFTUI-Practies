cbff6365de6199267252bdf6f5ac5e774d067c7f
Mobcoder <>
2020-06-30 23:51:42 +0530

    modify state and inding state

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -10,15 +10,54 @@ import SwiftUI
 
 struct ContentView: View {
 
-    //CREATE A FORM , WITH NAVIATION , SECTION AND GROUP .
+    /*
+     //Bind state to user interface
+     
+     1- Binding state means modify the value of property of struct by using *@State* property wrapper and bind with property wrapper . Example - Suppose you are creating textFiled and keep changing the value of textfiled it's also keep on changing  the struct property . It is called two way binding and achinving by using *$* symbol.
+     
+    */
 
-    // -- FORM--> FORM OF SCROLLING LISTS OF STATIC CONTROLS LIKE TEXT AND IMAGES , YOU CAN ALSO INCLUDE USER INTRACTION CONTROL LIKE BUTTON , TEXT FILED , TOGGLE AND MORE
     
-    //---GROUP --> SWIFT UI LIMITAION 10 CHILD VIEW INSIDE PARENT VIEW , BY USING GROUP YOU CAN OVERCOME FROM THIS LIMITATION
-   
-    //---SECTION --> DIVID THE UI WITH SECTION
-    
+    @State private var countt = 0
+    @State private var name = ""
+    @State private var password = ""
+
     var body: some View {
+        
+        Form {
+            Section {
+                TextField("Email address", text: $name)
+                TextField("Password",text: $password)
+            }
+            Section {
+                Button("Signup\(countt)") {
+                    self.countt += 1
+                }
+            }
+        }
+ 
+    }
+  
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+/* 28/06/2020
+ 
+
+  //CREATE A FORM , WITH NAVIATION , SECTION AND GROUP .
+
+  // -- FORM--> FORM OF SCROLLING LISTS OF STATIC CONTROLS LIKE TEXT AND IMAGES , YOU CAN ALSO INCLUDE USER INTRACTION CONTROL LIKE BUTTON , TEXT FILED , TOGGLE AND MORE
+  
+  //---GROUP --> SWIFT UI LIMITAION 10 CHILD VIEW INSIDE PARENT VIEW , BY USING GROUP YOU CAN OVERCOME FROM THIS LIMITATION
+ 
+  //---SECTION --> DIVID THE UI WITH SECTION
+ 
+ var body: some View {
         NavigationView{
             Form {
                 Section {
@@ -33,11 +72,20 @@ struct ContentView: View {
             }.navigationBarTitle(Text("SIGNUP") )
         }
     }
-  
-}
+ 
+ ------------------------------------------------------------------
+ 
+ 30 / 06 / 2020
+ 
+ // Modify the program state
+ 
+ 1 - In UIKit - You can change it by creating IBAction of button where as If talk about SwiftUI Its give you call back to get action of button
+ 2- There is one more intresting thing about the struct -
+    as we all know about if you want to modify strut property value than we have to use * mutating func *  where as Fortunately, Swift gives us a special solution called a property wrapper:
+    we can place *@State*  before our properties . we can use a property wrapper from SwiftUI called @State .
+ 
+ //Bind state to user interface
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+ 
+ 
+ */
